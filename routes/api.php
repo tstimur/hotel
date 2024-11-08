8db951946bc88e4fbcad494c3c8b9389fe6cc95a
Timur <cymzhitov.t@divan.ru>
2024-11-08 17:04:52 +0700

    fix: Configured api route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//Route::get('/v1/guests', [\App\Http\Controllers\Api\v1\GuestController::class, 'index']);
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('guests', \App\Http\Controllers\Api\v1\GuestController::class);
+});
