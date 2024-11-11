1e8cce16bb4021ea29eb34b0d8e409e126107dbe
Timur <cymzhitov.t@divan.ru>
2024-11-12 00:15:31 +0700

    feat: Added rendering exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +13,17 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Not found.'
+                ], 404);
+            }
+        });
+    })
+
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
