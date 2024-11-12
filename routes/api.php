b63bd6f5195f9b78c0c61eb40bdb65ea35975bba
Timur <cymzhitov.t@divan.ru>
2024-11-12 16:54:06 +0700

    feat: Added limit for api

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//Route::get('/v1/guests', [\App\Http\Controllers\Api\v1\GuestController::class, 'index']);
-
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware(['throttle:api'])->group(function () {
     Route::apiResource('guests', \App\Http\Controllers\Api\v1\GuestController::class);
 });
