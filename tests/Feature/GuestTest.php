eb1db75830f283dae017feb66557f9c04701d9c2
Timur <cymzhitov.t@divan.ru>
2024-11-13 01:49:35 +0700

    feat: Added test for guests api

---
+++ tests/Feature/GuestTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Guest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GuestTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+}
