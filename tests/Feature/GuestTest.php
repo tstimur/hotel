de4808297f48689e66ddf4357f3814a52ff73066
Timur <cymzhitov.t@divan.ru>
2024-11-13 01:50:50 +0700

    feat: Added show test

--- tests/Feature/GuestTest.php
+++ tests/Feature/GuestTest.php
@@ -17,5 +17,21 @@ class GuestTest extends TestCase
         $response->assertStatus(200);
     }
 
+    public function testGuestsShow()
+    {
+        $guest = Guest::factory()->create([
+            'first_name' => "Sayan",
+            'last_name' => "Shtolts",
+            'phone_number' => '+79295558899',
+            'email' => 'juice@mail.ru'
+        ]);
+
+        $response = $this->get("/api/v1/guests/{$guest->id}");
+
+        $response->assertStatus(200);
+
+        $response->assertJsonPath('selected_guest.first_name', $guest->name);
+    }
+
 
 }
