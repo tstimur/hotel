9c40ee37dbe707537b0e927b19cbc196d13effd6
Timur <cymzhitov.t@divan.ru>
2024-11-13 14:52:19 +0700

    feat: Added test GetCountryFromValidPhone

--- tests/Unit/GuestTest.php
+++ tests/Unit/GuestTest.php
@@ -2,15 +2,32 @@
 
 namespace Tests\Unit;
 
+use App\Http\Controllers\Api\v1\GuestController;
+use libphonenumber\PhoneNumberUtil;
 use PHPUnit\Framework\TestCase;
+use ReflectionMethod;
 
 class GuestTest extends TestCase
 {
     /**
      * A basic unit test example.
      */
-    public function test_example(): void
+    public function testGetCountryCodeFromValidPhone(): void
     {
-        $this->assertTrue(true);
+        $mockPhoneUntil = $this->createMock(PhoneNumberUtil::class);
+
+        $mockPhoneUntil->method('parse')
+            ->willReturnSelf();
+
+        $mockPhoneUntil->method('getRegionCodeForNumber')
+            ->willReturn('RU');
+
+        $controller = new GuestController();
+        $reflectionMethod = new ReflectionMethod(GuestController::class, 'getCountryCode');
+        $reflectionMethod->setAccessible(true);
+
+        $countryCode = $reflectionMethod->invoke($controller, '+79295558899');
+
+        $this->assertEquals('RU', $countryCode);
     }
 }
